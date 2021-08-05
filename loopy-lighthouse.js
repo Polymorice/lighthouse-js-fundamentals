9aea752a55f90aad135ee4651a68e575536323bd
Horace Zhao <horace.hoyu.w@gmail.com>
2021-08-05 06:07:15 +0000

    fixed loopy-lighthouse.js

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -1,5 +1,5 @@
-let number
-for (number = 100; number <=200; number++){
+let number;
+for (number = 100; number <= 200; number++){
 
   // use set diagram to figure out the conditions
   if (number % 3 !== 0 && number % 4 !== 0) {
@@ -7,7 +7,7 @@ for (number = 100; number <=200; number++){
   }
   // run the more strick condition first
   else if (number % 3 === 0 && number % 4 === 0) {
-    console.log('LoopyLighthouse')
+    console.log('LoopyLighthouse');
   }
   
   else if (number % 3 === 0) {
