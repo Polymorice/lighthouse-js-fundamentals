e904a697de92de5108025d1b62d9e000f36294bb
Horace Zhao <horace.hoyu.w@gmail.com>
2021-08-05 23:38:30 +0000

    added even.js

---
+++ even.js
@@ -0,0 +1,9 @@
+const isEven = function (num) {
+  return num % 2 === 0;
+};
+// assign
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
