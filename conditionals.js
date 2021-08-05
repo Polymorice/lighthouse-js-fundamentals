0e976e0dfe19b9a0e254c9a27a55b9996579a490
Horace Zhao <horace.hoyu.w@gmail.com>
2021-08-05 03:57:44 +0000

    added contionals.js

---
+++ conditionals.js
@@ -0,0 +1,25 @@
+// const raining = false;
+// const cold = true;
+
+// if (raining) {
+//   console.log("Don't forget your umbrella!");
+// }
+// // no else statment, no outcome for 'false'
+
+// if (cold) {
+//   console.log("Make sure you pick out a scarf!");
+// }
+
+// console.log("Now you're ready to go outside!");
+
+const temperature = 5;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
