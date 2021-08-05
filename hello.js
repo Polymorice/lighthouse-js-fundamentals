6beaa51cb08e9fc5953aeb9b203d528415d144d7
Horace Zhao <horace.hoyu.w@gmail.com>
2021-08-05 22:59:29 +0000

    added hello.js

---
+++ hello.js
@@ -0,0 +1,15 @@
+// static "hello, world"
+// const sayHello = function () {
+//   console.log('Hello, world');
+// }
+// sayHello();
+
+// greetings with input
+const sayHello = function (name) {
+  console.log("Hello, " + name);
+}
+
+// insert value for name as invoking the function
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
