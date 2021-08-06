03c0c7fa8e738ba805766fdc160aeee6ed51780d
Horace Zhao <horace.hoyu.w@gmail.com>
2021-08-06 15:20:29 +0000

    added ageCalculator.js

---
+++ ageCalculator.js
@@ -0,0 +1,15 @@
+function ageCalculator(name, yearB, yearC) {
+  // let name;
+  // let yearB;
+  // let yearC;
+  return name + "is " + (yearC - yearB) + " years old.";
+}
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
+
+// test
+// function printf(a, b, c) {
+//   return a + ", " + b + ", " + c;
+// }
+
+// console.log(printf("1", "2", "9"));
